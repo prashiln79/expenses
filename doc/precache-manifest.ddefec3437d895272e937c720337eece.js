7e0294c04c3571bf21195d895febc363ad6bf2b0
Prashil <Prashiln79@gmail.com>
2021-08-08 12:12:27 +0530

    updated js changes

---
+++ doc/precache-manifest.ddefec3437d895272e937c720337eece.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e55fb2554423a210a13c3e6f8e33566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ae5b7a13264cd5d19aa",
+    "url": "/static/css/2.7ac39a5d.chunk.css"
+  },
+  {
+    "revision": "f04f1df6c760a4ec3eea",
+    "url": "/static/css/main.6771c0cf.chunk.css"
+  },
+  {
+    "revision": "6ae5b7a13264cd5d19aa",
+    "url": "/static/js/2.fa8636d4.chunk.js"
+  },
+  {
+    "revision": "f14083e6e3fa69b0fba330ce12367a6d",
+    "url": "/static/js/2.fa8636d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04f1df6c760a4ec3eea",
+    "url": "/static/js/main.2648db18.chunk.js"
+  },
+  {
+    "revision": "f31b34e9a7a60b9e51c0",
+    "url": "/static/js/runtime-main.e901c6d6.js"
+  }
+]);
